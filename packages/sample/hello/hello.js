2bcc0d57e05500ca3565fc25f281736a5d1ee6ff
techswarn <tech.swarns@gmail.com>
2024-05-28 13:22:58 +0530

    code update

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,8 +1,21 @@
 function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
+  let name = args.name || "stranger";
+  let greeting = "Hello " + name + "!";
+  console.log(greeting);
+  try {
+    fetch("https://go.techenv.dev/api/v1/") // api for the get request
+      .then((response) => {
+        response.json();
+        console.log(response.json());
+      })
+      .then((data) => {
+        console.log(data);
+        return { body: { success: true, data: data }, headers: { foo: "bar" } };
+      });
+  } catch (err) {
+    console.log(err);
+    return { body: { success: false, data: err }, headers: { foo: "bar" } };
   }
+}
 
-exports.main = main
+exports.main = main;
